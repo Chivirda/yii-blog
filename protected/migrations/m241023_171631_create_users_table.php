fb8b5a418986e8d938adf65d91bfc978484afada
Sergey Chivirda <si.chivirda@gmail.com>
2024-10-24 08:07:03 +0300

    feat: сделал первую миграцию для модели User

---
+++ protected/migrations/m241023_171631_create_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+class m241023_171631_create_users_table extends CDbMigration
+{
+	public function up()
+	{
+		$this->createTable('users', array(
+			'id' => 'pk',
+			'username' => 'string NOT NULL',
+			'email' => 'string NOT NULL',
+			'password' => 'string NOT NULL',
+		));
+	}
+
+	public function down()
+	{
+		$this->dropTable('users');
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
